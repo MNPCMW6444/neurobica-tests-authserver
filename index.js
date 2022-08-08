b5e3810fc983644a2aba2a9d703456d4fad235cc
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-08-08 15:48:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://test.neurobica.online"],
+    origin: ["http://localhost:3000", "https://testc.neurobica.online"],
     credentials: true,
   })
 );
